45fbc0282d84e493e33121926cb80ac1cef249e0
Ian <ian@fourthsailcap.com>
2024-10-04 11:38:38 -0300

    added cv and publications in new style

---
+++ javascript/pubs_ham.js
@@ -0,0 +1,14 @@
+// scripts.js
+
+function toggleMenu() {
+    var sidebar = document.getElementById("sidebar");
+    var mainContent = document.getElementById("main-content");
+
+    if (sidebar.style.width === "250px") {
+        sidebar.style.width = "0";
+        mainContent.style.marginRight = "0"; // Cambiado a margin-right
+    } else {
+        sidebar.style.width = "250px";
+        mainContent.style.marginRight = "250px"; // Cambiado a margin-right
+    }
+}
